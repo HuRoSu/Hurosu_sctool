29ee2dee1ee0b08d248161dd991d50227f92a8dc
σ`∀´)σ <hitdoomed@gmail.com>
2020-09-14 15:21:33 +0800

    Create server.py

---
+++ server.py
@@ -0,0 +1,32 @@
+import socket
+import time
+
+ip_port = ('127.0.0.1', 9999)
+sk = socket.socket()          
+sk.bind(ip_port)
+print("-"*10)
+print("[+] Setting Done!")
+print("[+] Wating...")
+sk.listen(5)
+conn, address = sk.accept()
+print("[+] Connect!")
+print("[+] IP:",address[0])
+print("[+] Port:",address[1])
+print("[+] Type help for help.")
+while True:
+    data = input("Control "+address[0]+" :")
+    if data == 'exit':
+        conn.send(data.encode())
+        print("[X] Connect Close")
+        conn.close()
+        break
+    elif data == 'help':
+        print("exit to quit")
+    elif data != '':
+        data = (data+" && echo [+] Command Request Done!")
+        conn.send(data.encode())
+        print(conn.recv(4096).decode('big5'))
+    elif data == '':
+        print("pls enter")
+        continue
+conn.close()
